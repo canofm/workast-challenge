3c959351fe3d3c5c87958935db9d773324ec66a1
canofm <canofedericomartin@gmail.com>
2019-02-25 11:22:55 -0300

    Adding config

---
+++ src/config/index.js
@@ -0,0 +1,30 @@
+import { merge } from "lodash";
+
+const env = process.env.NODE_ENV || "development";
+
+const baseConfig = {
+  port: process.env.PORT || 3000,
+  db: {
+    url: "mongodb://localhost/workast-articles"
+  },
+  api: {
+    baseUri: "/api/v0"
+  }
+};
+
+let envConfig = {};
+
+switch (env) {
+  case "development":
+  case "dev":
+    envConfig = require("./development").config;
+    break;
+  case "prod":
+  case "production":
+    envConfig = require("./production").config;
+    break;
+  default:
+    envConfig = require("./development").config;
+}
+
+export default merge(baseConfig, envConfig);
--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
 require("@babel/register");
 require("@babel/polyfill");
+import http from "http";
+import app from "./server";
+import config from "./config";
+
+const server = http.createServer(app);
+
+server.listen(
+  config.port,
+  () => console.log(`Server listing on port ${config.port}`) /* eslint-disable-line no-console */
+);
