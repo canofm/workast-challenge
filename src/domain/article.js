a4bc7222567be9a53f14f56e8883889b7ef44240
canofm <canofedericomartin@gmail.com>
2019-02-27 02:24:49 -0300

    Adding Article domain object

---
+++ src/domain/article.js
@@ -0,0 +1,69 @@
+import { isEmpty } from "lodash";
+import { PropertyRequiredException, BuilderException } from "../exceptions";
+
+class Article {
+  constructor(build) {
+    if (!build) {
+      throw new BuilderException("Article");
+    }
+
+    this.userId = build._userId;
+    this.title = build._title;
+    this.title = build._title;
+    this.text = build._text;
+    this.tags = build._tags;
+    if (!isEmpty(build._id)) this.id = build._id;
+  }
+
+  static get Builder() {
+    class Builder {
+      id(id) {
+        this._id = id;
+        return this;
+      }
+
+      userId(userId) {
+        this._userId = userId;
+        return this;
+      }
+
+      title(title) {
+        this._title = title;
+        return this;
+      }
+
+      text(text) {
+        this._text = text;
+        return this;
+      }
+
+      tags(tags) {
+        this._tags = tags;
+        return this;
+      }
+
+      build() {
+        if (isEmpty(this._userId)) {
+          throw new PropertyRequiredException("Article", "userId");
+        }
+
+        if (isEmpty(this._title)) {
+          throw new PropertyRequiredException("Article", "title");
+        }
+
+        if (isEmpty(this._text)) {
+          throw new PropertyRequiredException("Article", "text");
+        }
+
+        if (isEmpty(this._tags)) {
+          throw new PropertyRequiredException("Article", "tags");
+        }
+
+        return new Article(this);
+      }
+    }
+    return Builder;
+  }
+}
+
+export default Article;
---
+++ src/domain/tests/article.test.js
@@ -0,0 +1,73 @@
+import { expect } from "chai";
+import { PropertyRequiredException, BuilderException } from "../../exceptions";
+import Article from "../article";
+
+describe("Article", () => {
+  describe("when building", () => {
+    const userId = "asdasdadsadasd123";
+    const title = "aTitle";
+    const text = "aText";
+    const tags = ["aTag", "otherTag"];
+
+    it("must be created through builder", () => {
+      expect(() => new Article()).to.throw(BuilderException);
+    });
+
+    it("must have a userId", () => {
+      expect(() =>
+        new Article.Builder()
+          .title(title)
+          .text(text)
+          .tags(tags)
+          .build()
+      ).to.throw(PropertyRequiredException);
+    });
+
+    it("must have a title", () => {
+      expect(() =>
+        new Article.Builder()
+          .userId(userId)
+          .text(text)
+          .tags(tags)
+          .build()
+      ).to.throw(PropertyRequiredException);
+    });
+
+    it("must have a text", () => {
+      expect(() =>
+        new Article.Builder()
+          .userId(userId)
+          .title(title)
+          .tags(tags)
+          .build()
+      ).to.throw(PropertyRequiredException);
+    });
+
+    it("must have at least one tag", () => {
+      expect(() =>
+        new Article.Builder()
+          .userId(userId)
+          .title(title)
+          .text(text)
+          .build()
+      ).to.throw(PropertyRequiredException);
+    });
+
+    it("if it has every required property should be ok", () => {
+      const id = "c37d82c9-6b50-4707-99e7-180b661965c3"; // random uuid
+      const article = new Article.Builder()
+        .userId(userId)
+        .title(title)
+        .text(text)
+        .tags(tags)
+        .id(id)
+        .build();
+
+      expect(article.userId).to.be.eql(userId);
+      expect(article.title).to.be.eql(title);
+      expect(article.text).to.be.eql(text);
+      expect(article.tags).to.be.eql(tags);
+      expect(article.id).to.be.eql(id);
+    });
+  });
+});
