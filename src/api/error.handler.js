c5b5f79f1b216c86db1680d3693f000f5addf809
canofm <canofedericomartin@gmail.com>
2019-03-03 18:04:21 -0300

    Adding swagger documentation

--- src/api/error.handler.js
+++ src/api/error.handler.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line
 export const ErrorHandler = (error, req, res, next) => {
-  console.log({ error });
+  // console.log({ error });
   // console.error(error.stack); // TODO: Enable log just for production
   res.status(error.statusCode || 500).send(error.message || error.toString());
 };
